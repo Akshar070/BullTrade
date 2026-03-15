64480b9872d56b8420775fce9264717586114fed
Akshar <144157858+Akshar070@users.noreply.github.com>
2026-03-15 14:37:33 +0530

    Updated the predict

--- backend/predict.py
+++ backend/predict.py
@@ -5,39 +5,38 @@ import pandas as pd
 lr_model = joblib.load("models/linear_model.pkl")
 rf_model = joblib.load("models/rf_model.pkl")
 
+
 def predict_stock(stock):
 
-    data = yf.download(stock, period="3mo")
+    data = yf.download(stock, period="1y")
 
     if data.empty:
-        return {"error":"No stock data"}
+        return {"error": "No data found"}
 
-    data['Prev Close'] = data['Close'].shift(1)
-    data['MA10'] = data['Close'].rolling(10).mean()
-    data['MA50'] = data['Close'].rolling(50).mean()
+    data["MA10"] = data["Close"].rolling(10).mean()
+    data["MA50"] = data["Close"].rolling(50).mean()
 
-    data = data.dropna()
+    data.dropna(inplace=True)
 
     latest = data.iloc[-1]
 
     features = [[
-        float(latest['Prev Close']),
-        float(latest['MA10']),
-        float(latest['MA50']),
-        float(latest['Volume'])
+        float(latest["Close"]),
+        float(latest["MA10"]),
+        float(latest["MA50"]),
+        float(latest["Volume"])
     ]]
 
     lr_pred = lr_model.predict(features)[0]
     rf_pred = rf_model.predict(features)[0]
-    
+
     current_price = float(latest["Close"])
 
     direction = "Rise 📈" if rf_pred > current_price else "Fall 📉"
 
     return {
-        "current_price":current_price,
-        "lr_prediction":float(lr_pred),
-        "rf_prediction":float(rf_pred),
-        "direction":direction,
-        "time": data.index[-1].strftime("%Y-%m-%d")
+        "current_price": round(current_price,2),
+        "lr_prediction": round(float(lr_pred),2),
+        "rf_prediction": round(float(rf_pred),2),
+        "direction": direction
     }
