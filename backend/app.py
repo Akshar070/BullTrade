7a788e13217dc88073bc56bbfa76eae829df0e86
Akshar <144157858+Akshar070@users.noreply.github.com>
2026-03-15 13:58:56 +0530

    Updated the app

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, jsonify
 from flask_cors import CORS
 import yfinance as yf
@@ -184,6 +186,5 @@ def live_price(stock):
         return jsonify({"error": str(e)})
 
 if __name__ == "__main__":
-    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
