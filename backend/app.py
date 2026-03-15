dc39047f4d71b4913f5968217944a38bd07182e5
Akshar <144157858+Akshar070@users.noreply.github.com>
2026-03-15 12:51:30 +0530

    app changed

--- backend/app.py
+++ backend/app.py
@@ -174,4 +174,6 @@ def live_price(stock):
         return jsonify({"error": str(e)})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
